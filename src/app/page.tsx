76349d4e7d7f584f67308c6b1dcca0c1343c0017
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-01-19 18:15:55 +0900

    fix: 배포 오류 부분 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import Title from '../components/common/Title';
 
 export default function Page() {
   return (
-    <div >
+    <div>
       <Title>Jinsoul Kim</Title>
+      <div>
+        <div>안녕하세요 Frontend Develper 김진솔입니다.</div>
+        <div>다음과 같은 가치를 두며 꾸준히 성장을 이뤄나가고 있습니다.</div>
+        <div>호기심을 가지고 깊이 탐구 하는것</div>
+        <div>문제를 인식하는데 그치지 않고 해결하는 것</div>
+        <div>그 과정에서 얻은 지식을 공유하고 함께 성장하는것</div>
+      </div>
     </div>
   );
 }
