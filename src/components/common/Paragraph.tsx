e940803407be11078cb66b2b788b51219327b560
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-02-02 17:22:19 +0900

    fix: 배포 오류 부분 수정

--- src/components/common/Paragraph.tsx
+++ src/components/common/Paragraph.tsx
@@ -1,4 +1,15 @@
-export default function Paragraph({ children, className, ...props }) {
+import { ReactNode } from 'react';
+
+interface ParagraphProps {
+  className: string;
+  children: ReactNode;
+}
+
+export default function Paragraph({
+  children,
+  className,
+  ...props
+}: ParagraphProps) {
   return (
     <p className={`${className} break-words`} {...props}>
       {children}
