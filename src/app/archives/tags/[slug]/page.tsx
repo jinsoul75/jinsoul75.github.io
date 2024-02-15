4291481b59a748561ea5b5b239d6e1dc938a2903
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-02-15 14:29:45 +0900

    feat: tags 페이지 SSG

--- src/app/archives/tags/[slug]/page.tsx
+++ src/app/archives/tags/[slug]/page.tsx
@@ -3,6 +3,13 @@ import { ParamsProps } from '@/types/types';
 import { getPostsByTag } from '@/constants/dataset';
 import PostList from '@/components/common/PostList';
 import { notFound } from 'next/navigation';
+import { allBlogPosts } from '@/constants/dataset';
+
+export function generateStaticParams() {
+  return [...allBlogPosts].map((blogPost) => ({
+    slug: blogPost.slugAsParams,
+  }));
+}
 
 export default function Tags({ params }: ParamsProps) {
   const posts = getPostsByTag(params.slug);
