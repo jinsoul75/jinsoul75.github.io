75b589d9401fbeeeb0b460c88c917ef9055db8a7
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-02-15 14:12:40 +0900

    chore: 검색바 컴포넌트 생성

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,13 +5,14 @@ import SubTitle from '@/components/common/SubTitle';
 import Paragraph from '@/components/common/Paragraph';
 import PostList from '@/components/common/PostList';
 import SeriesCard from '@/components/card/SeriesCard';
+import SearchBar from '@/components/searchBar/SearchBar';
 
 export default function BlogPage() {
   return (
     <>
       <Title>Blog</Title>
       <Paragraph className="pb-4">프론트엔드 세계를 탐구합니다.</Paragraph>
-      <div>검색바 컴포넌트</div>
+      <SearchBar/>
       <SubTitle>Series</SubTitle>
       <SeriesCard />
       <SubTitle>All Posts ({allBlogPosts.length})</SubTitle>
--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,3 +1,17 @@
-export default function SearchBar(){
-    
+import { cn } from '@/libs/utils';
+import { CiSearch } from 'react-icons/ci';
+
+export default function SearchBar() {
+  const className = cn(
+    'border border-black p-2 w-full',
+    'focus:ring-2',
+    'ring-black',
+    'outline-none',
+  );
+  return (
+    <div className='relative'>
+      <input placeholder="검색기능 준비중" className={className} />
+      <CiSearch className='absolute right-4 top-2' size={24}/>
+    </div>
+  );
 }
