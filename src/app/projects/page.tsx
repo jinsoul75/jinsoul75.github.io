f8fc09da6718122ee980ca3a2b00ba23b3da7941
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-02-19 16:57:26 +0900

    set: husky 설정

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 import { allProjects } from 'contentlayer/generated';
 
 import Title from '@/components/common/Title';
-import Paragraph from '@/components/common/Paragraph'
+import Paragraph from '@/components/common/Paragraph';
 import PostList from '@/components/common/PostList';
 
 export default function Projects() {
@@ -9,9 +9,9 @@ export default function Projects() {
     <>
       <Title>Projects</Title>
 
-      <Paragraph className='pb-4'>진행했던 프로젝트 목록입니다.</Paragraph>
+      <Paragraph className="pb-4">진행했던 프로젝트 목록입니다.</Paragraph>
 
-      <PostList posts={allProjects}/>
+      <PostList posts={allProjects} />
     </>
   );
 }
