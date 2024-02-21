ea37ba47fa1f32bccdfd194d83e4865b1bc3011e
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-02-21 17:15:51 +0900

    style: 서치 컴포넌트 스타일링 변경

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -3,15 +3,21 @@ import { CiSearch } from 'react-icons/ci';
 
 export default function SearchBar() {
   const className = cn(
-    'border border-black p-2 w-full',
-    'focus:ring-2',
+    'bg-gray-100 p-2 w-full flex',
+    'hover:ring-1',
+    'focus:ring-1',
     'ring-black',
     'outline-none',
+    'rounded-2xl',
   );
+
   return (
-    <div className='relative'>
-      <input placeholder="검색기능 준비중" className={className} />
-      <CiSearch className='absolute right-4 top-2' size={24}/>
+    <div className={className}>
+      <CiSearch className="mx-2" size={24} />
+      <input
+        className="w-full bg-gray-100 focus:outline-none"
+        placeholder="검색기능 준비중"
+      />
     </div>
   );
 }
