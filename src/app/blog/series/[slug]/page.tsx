b8074c25ea4a0f5e84ca5d1267d28a1e232d21ca
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-02-26 21:41:29 +0900

    remove: ssg경로 삭제

--- src/app/blog/series/[slug]/page.tsx
+++ src/app/blog/series/[slug]/page.tsx
@@ -2,14 +2,8 @@ import Image from 'next/image';
 
 import { ParamsProps } from '@/types/types';
 import { SERIES_INFO } from '@/constants/route';
-import { allSeriesPosts, getSeriesPostsBySlug } from '@/constants/dataset';
+import { getSeriesPostsBySlug } from '@/constants/dataset';
 import PostList from '@/components/common/PostList';
-
-export async function generateStaticParams() {
-  return [...allSeriesPosts].map((blogPost) => ({
-    slug: blogPost.slugAsParams,
-  }));
-}
 
 export default function Series({ params }: ParamsProps) {
   const seriesPosts = getSeriesPostsBySlug(params?.slug);
