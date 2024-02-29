b54ecb2254ebefe04c658ad73af13e15b788e988
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-02-29 23:21:58 +0900

    style: 메인 페이지 스켈레톤 UI 변경

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -10,10 +10,10 @@ export default function Loading() {
       <div className="flex flex-col gap-4">
         <div className="w-20 h-6 bg-gray-200"></div>
         <div className="grid grid-cols-4 gap-4">
-          <div className="w-20 bg-gray-200"></div>
-          <div className="w-20 bg-gray-200"></div>
-          <div className="w-20 bg-gray-200"></div>
-          <div className="w-20 bg-gray-200"></div>
+          <div className="w-60 h-60 bg-gray-200"></div>
+          <div className="w-60 h-60 bg-gray-200"></div>
+          <div className="w-60 h-60 bg-gray-200"></div>
+          <div className="w-60 h-60 bg-gray-200"></div>
         </div>
       </div>
     </div>
