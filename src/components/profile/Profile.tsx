a4d47904d426abd9827d835a5b76742538175e3f
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-03-05 10:40:14 +0900

    chore: 프로필 사진 깃허브 연동

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
+
 import LinkContacts from '../common/LinkContacts';
+import { profileAvatar } from '@/constants/imgUrl';
 
 export default function Profile() {
   return (
     <section className="flex justify-center items-center">
-      <figure className="mr-4 w-[20px] h-[20x]">
+      <figure className="mr-4 w-[80px] h-[80x]">
         <Image
-          height={0}
-          width={0}
-          sizes="100vw"
-          src="https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8JUVBJUIwJTk1JUVDJTk1JTg0JUVDJUE3JTgwfGVufDB8fDB8fHww"
+          height={80}
+          width={80}
+          src={profileAvatar}
           alt="profile"
           className="rounded-full"
           priority
---
+++ src/constants/imgUrl.ts
@@ -0,0 +1,2 @@
+export const profileAvatar =
+  'https://avatars.githubusercontent.com/u/80370226?v=4';
