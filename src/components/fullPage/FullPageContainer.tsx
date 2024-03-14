9071c7b771e5d09442947f0d938ed7e9c81e8ac2
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-03-14 15:49:20 +0900

    feat: scroll snap

---
+++ src/components/fullPage/FullPageContainer.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import { useRef, useEffect, useState, WheelEvent } from 'react';
+import FirstPage from './FirstPage';
+import SecondPage from './SecondPage';
+import FullPageController from './FullPageController';
+
+export default function FullPageContainer({
+  className,
+}: {
+  className: string;
+}) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const outerDivRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const wheelHandler: EventListenerOrEventListenerObject = (event) => {
+      event.preventDefault();
+      const wheelEvent = event as unknown as WheelEvent<HTMLDivElement>;
+      const { deltaY } = wheelEvent;
+
+      const pageHeight = window.innerHeight;
+      if (outerDivRef.current) {
+        const { scrollTop } = outerDivRef.current;
+
+        if (deltaY > 0) {
+          // 스크롤 내릴 때
+          if (scrollTop >= 0 && scrollTop < pageHeight) {
+            setCurrentPage(1);
+            //현재 1페이지
+            console.log('현재 1페이지, down');
+            outerDivRef.current.scrollTo({
+              top: pageHeight,
+              left: 0,
+              behavior: 'smooth',
+            });
+          } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+            //현재 2페이지
+            setCurrentPage(2);
+            console.log('현재 2페이지, down');
+            outerDivRef.current.scrollTo({
+              top: pageHeight * 2,
+              left: 0,
+              behavior: 'smooth',
+            });
+          } else {
+            // 현재 3페이지
+            setCurrentPage(3);
+            console.log('현재 3페이지, down');
+            outerDivRef.current.scrollTo({
+              top: pageHeight * 2,
+              left: 0,
+              behavior: 'smooth',
+            });
+          }
+        } else {
+          // 스크롤 올릴 때
+          if (scrollTop > pageHeight && scrollTop <= pageHeight * 2) {
+            // 현재 3페이지
+            console.log('현재 3페이지, up');
+            outerDivRef.current.scrollTo({
+              top: pageHeight,
+              left: 0,
+              behavior: 'smooth',
+            });
+          } else if (
+            scrollTop !== 0 &&
+            scrollTop <= pageHeight &&
+            scrollTop <= pageHeight * 2
+          ) {
+            //현재 2페이지
+            console.log('현재 2페이지, up');
+            outerDivRef.current.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: 'smooth',
+            });
+          } else {
+            //현재 1페이지
+            console.log('현재 1페이지, up');
+            outerDivRef.current.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: 'smooth',
+            });
+          }
+        }
+      }
+    };
+
+    const outerDivRefCurrent = outerDivRef.current;
+
+    outerDivRefCurrent?.addEventListener('wheel', wheelHandler);
+
+    return () => {
+      outerDivRefCurrent?.removeEventListener('wheel', wheelHandler);
+    };
+  }, []);
+
+  return (
+    <div ref={outerDivRef} className={className}>
+      <FirstPage />
+      <SecondPage />
+      <FirstPage />
+      <FullPageController currentPage={currentPage} />
+    </div>
+  );
+}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import HamburgerButton from './HamburgerButton';
 
 export default function Header() {
   return (
-    <header className="pt-10 pb-5 border-b border-b-gray bg-white z-10 dark:bg-black">
+    <header className="pt-10 pb-5 border-b border-b-gray bg-white z-10 dark:bg-black fixed left-0 right-0">
       <nav className="flex justify-end sm:justify-between">
         <NavBar />
         <div className="flex gap-2">
